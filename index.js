26bbd9d077de9d7a19d16127c7cbce60167c3a43
lakesman19 <102413469+lakesman19@users.noreply.github.com>
2022-06-02 20:01:04 +0100

    up

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const sideMenu = document.querySelector('aside');
 const menuBtn = document.querySelector('.btn-menu');
 const closeBtn = document.querySelector('#close');
 const themeToggle = document.querySelector('.theme-toggle');
+const body = document.querySelector('#container')
 
 
 menuBtn.addEventListener('click' , () => {
@@ -10,7 +11,24 @@ menuBtn.addEventListener('click' , () => {
 closeBtn.addEventListener('click' , () => {
     sideMenu.style.display ='none';
 })
+// body.addEventListener('click' , () => {
+//     sideMenu.style.display ='none';
+// })
+
 
 // themeToggle.addEventListener('click' , () => {
 //     themeToggle.body.color ='black'
 // })
+
+
+
+const age = prompt('enter age ');
+// const isOldEnough = age > 18;
+// const legalAge = 18
+
+if(age > 18) {alert('sarah is old enough to start driving')}
+else if (age <= 18 && age >= 18 ){alert('sarah needs to take a driving lesson')}
+else {
+         age < 18
+    alert('sarah is too young to drive. wait another; (alert(years)) years')
+}
